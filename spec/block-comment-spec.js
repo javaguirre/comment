fe1ab9f31c408a3cbc791d764495f98e9dbfb948
havber <havber@gmail.com>
2014-08-05 19:06:14 +0200

    Added test framework

---
+++ spec/block-comment-spec.js
@@ -0,0 +1,9 @@
+var bc = require('../lib/block-comment.js');
+
+describe("block-command is an api", function() {
+
+    it("toggle should be a function", function() {
+        expect(true).toBe(true);
+    });
+
+});
