0f1445a28acae7bbfb87057ee761e3b27b962ea9
havber <havber@gmail.com>
2014-08-05 19:18:14 +0200

    Wrote some tests

--- spec/block-comment-spec.js
+++ spec/block-comment-spec.js
@@ -1,9 +1,13 @@
 var bc = require('../lib/block-comment.js');
 
-describe("block-command is an api", function() {
+describe("block-command exposes", function() {
 
-    it("toggle should be a function", function() {
-        expect(true).toBe(true);
+    it("activate() as a public function", function() {
+        expect(typeof bc.toggle).toEqual('function');
+    });
+
+    it("toggle() as a public function", function() {
+        expect(typeof bc.toggle).toEqual('function');
     });
 
 });
